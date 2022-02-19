7db10fb3878d24c84d394d53ca335ff848cc4479
Chad <chadjpeterson21@gmail.com>
2022-02-19 17:53:52 -0500

    initial upload
    added movedex folder, existing code for basic move object and tester to make sure it works. pipfile and pipfile.lock can be ignored.

---
+++ MoveDex/code/move.py
@@ -0,0 +1,20 @@
+from prettytable import PrettyTable
+import pokepy as ppy
+
+
+class Move:
+
+    def __init__(self):
+        self.id = 0
+        self.name = ""
+        self.accuracy = 0
+        self.pp = 0
+        self.priority = 0  # might cut
+        self.power = 0
+
+    def __str__(self):
+        pt = PrettyTable()
+        pt.field_names = ["ID", "Name", "Accuracy", "PP", "Priority", "Power"]
+        pt.add_row([self.id, self.name, self.accuracy, self.pp, self.priority,
+                    self.power])
+        return pt.__str__()
---
+++ MoveDex/code/test.py
@@ -0,0 +1,5 @@
+from move import Move
+
+test_move = Move()
+Move.id, Move.name, Move.accuracy, Move.pp, Move.priority, Move.power = 1, "", 1, 1, 1, 1
+print(test_move)
