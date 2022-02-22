3d8ae4072a180945188c77f6083df0417bdcb5fd
clduke2 <71196463+clduke2@users.noreply.github.com>
2022-02-22 15:19:56 -0500

    Add files via upload

---
+++ Trainer/scripts.py
@@ -0,0 +1,17 @@
+# Christopher Duke
+# TeamDex/scripts.py
+
+import csv
+
+
+def save_team_to_csv(pokemon_team):
+    header = ['#', 'Pokemon', 'Held Item', 'Move 1', 'Move 2', 'Move 3', 'Move 4']
+
+    with open('TeamDex\\team.csv', 'w', newline='', encoding='UTF8') as f:
+        writer = csv.writer(f)
+
+        writer.writerow(header)
+
+        for count, pokemon in enumerate(pokemon_team.members):
+            writer.writerow([count + 1, pokemon.get_name(), pokemon.get_item(),
+                             pokemon.get_move1(), pokemon.get_move2(), pokemon.get_move3(), pokemon.get_move4()])
