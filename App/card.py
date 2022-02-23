91364d3ced26b09f05b48290a41430e38f10f4dd
AJohnsonWakeTech <75625961+AJohnsonWakeTech@users.noreply.github.com>
2022-02-23 00:08:30 -0500

    Add files via upload

--- App/card.py
+++ App/card.py
@@ -1,8 +1,15 @@
-"""This is the class that's going to pull one thing from the database
-and put it in a format the UI can use."""
+"""
+    This is the class that's going to pull one thing from the database
+    and put it in a format the UI can use.
+    The asset ID is being stored in self.number
+"""
 class Card:
 
     def __init__(self, id):
         self.number = str(id)
-        self.name = "mew"
-        self. description = "mew is cute"
+        pokeIDName = ["MissingNo", "Bulbasaur", "Ivysaur", "Venusaur","Poke Ball", "Town Map", "Bicycle", "??????", " ", " ","Scratch", "Vise Grip", "Guillotine", "Razor Wind"]
+        pokeIDDesc = ["The glitch Pokemon", "The plant Pokemon", "An Evolved Pokemon", "Watch Out!",
+                      "A Ball thrown at wild Pokémon to catch them.", "A map of the local area. It identifies your present location.",
+                      "Lets you travel twice as fast as walking.", "Allows surfing over water without the need for Pokémon and/or Badges.", " ", " ","Normal", "Normal", "Normal", "Normal"]
+        self.name = pokeIDName[id]
+        self. description = pokeIDDesc[id]
--- App/main.py
+++ App/main.py
@@ -8,27 +8,35 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
 from kivy.uix.button import Button
 from kivy.core.window import Window
+from functools import partial
 
 from card import Card
 
 Window.clearcolor = (1, 1, 1, 1)
 Window.size = (540, 1140)
 
+""" 
+    This class creates a card for each pokemon/move/item that stores its id
+    It generates a button for the asset to be displayed on the appropriate screen
+    When the button is clicked, it creates a popup displaying the details about the asset
+    Note: Nothing in here should need to be touched to connect to DBs
+ """
 class PokemonCard(BoxLayout):
     def __init__(self, id):
         self.id = id
- 
     
-    def GetCardData(self, id):
-        myCard = Card(id)
+    def GetCardData(self, *args):
+        print(self.id)
+        myCard = Card(self.id)
         content = BoxLayout(orientation="vertical")
         self.popup = Popup(title=myCard.name, size_hint=(None, None),
                            size=(400, 400), auto_dismiss=False, content=content)
         numberLabel = Label(text=myCard.number)
         nameLabel = Label(text=myCard.name)
         descLabel = Label(text=myCard.description)
-        """This is the line that would add to trainer"""
+     
         button = Button(text="Add")
+        button.bind(on_release=partial(self.AddToTrainer))
         close_btn = Button(text="Close", on_press=self.popup.dismiss)
         content.add_widget(numberLabel)
         content.add_widget(nameLabel)
@@ -36,34 +44,97 @@ class PokemonCard(BoxLayout):
         content.add_widget(button)
         content.add_widget(close_btn)
         self.popup.open()
-
-    def CreatePokeButton(self, poke_id):
+ 
+    def CreatePokeButton(self, poke_id, poke_name):
         self.id = poke_id
-        content = BoxLayout(orientation="vertical")
-        button = Button(text=str(self.id))
-        content.add_widget(button)
-    def method(self, number):
-        self.label.text = str(number)
-    pass
+        button = Button(text=str(poke_id) + " " + poke_name, size_hint=(.95, None), size=(100,40), pos_hint={'center_x': .5})
+        button.bind(on_release=partial(self.GetCardData))
+        return button
+
+    def AddToTrainer(self, *args):
+        print (self.id)
 
 class ContentNavigationDrawer(BoxLayout):
     pass
 
-class CardPopUp(Popup):
-    
-    pass
-
+""" 
+    This class controls the pokemon buttons on the PokeDex screen
+    This needs to be hooked up to the DB with a list of IDs and Names
+ """
 class PokeDex(BoxLayout):
     
     def GeneratePokemon(self):
-        pokeList = [1,2,3]
-        for poke in pokeList:
-            content = BoxLayout(orientation="vertical")
-            button = Button(text=str(poke))
-            content.add_widget(button)
-            print(poke)
+        
+        """These two arrays are taking place of reading the data from the DB"""
+        pokeIDList = [0,1,2,3]
+        pokeIDName = ["MissingNo", "Bulbasaur", "Ivysaur", "Venusaur"]
+        """The above arrays need to be fixed to access dbs"""
+
+        for poke in pokeIDList:
+            pCard = PokemonCard(poke)
+            button =pCard.CreatePokeButton(poke, pokeIDName[poke])
+            self.ids.poke.add_widget(button)
     pass
 
+""" 
+    This class controls the move buttons on the PokeDex screen
+    This needs to be hooked up to the DB with a list of IDs and Names
+ """
+class MoveDex(BoxLayout):
+    
+    def GenerateMoves(self):
+
+        """These two arrays are taking place of reading the data from the DB"""
+        pokeIDList = [0,1,2,3]
+        moveIDList = [10,11,12,13]
+        moveIDName = ["Scratch", "Vise Grip", "Guillotine", "Razor Wind"]
+        """The above arrays need to be fixed to access dbs"""
+
+        for move in pokeIDList:
+            mCard = PokemonCard(moveIDList[move])
+            button =mCard.CreatePokeButton(moveIDList[move], moveIDName[move])
+            self.ids.move.add_widget(button)
+    pass
+
+""" 
+    This class controls the item buttons on the PokeDex screen
+    This needs to be hooked up to the DB with a list of IDs and Names
+ """
+class ItemDex(BoxLayout):
+    
+    def GenerateItems(self):
+        
+        """These two arrays are taking place of reading the data from the DB"""
+        pokeIDList = [0,1,2,3]
+        itemIDList = [4,5,6,7]
+        itemIDName = ["Poke Ball", "Town Map", "Bicycle", "??????"]
+        """The above arrays need to be fixed to access dbs"""
+
+        for item in pokeIDList:
+            iCard = PokemonCard(itemIDList[item])
+            button =iCard.CreatePokeButton(itemIDList[item], itemIDName[item])
+            self.ids.item.add_widget(button)
+    pass
+
+""" 
+    This class controls the team buttons on the PokeDex screen
+    This needs to be hooked up to where the team info is being stored
+ """
+class Trainer(BoxLayout):
+    
+    def GenerateTeam(self):
+
+        """These two arrays are taking place of reading the data from the DB"""
+        pokeIDList = [0,1,2,3]
+        pokeIDName = ["MissingNo", "Bulbasaur", "Ivysaur", "Venusaur"]
+        """The above arrays need to be fixed to access dbs"""
+
+        for poke in pokeIDList:
+            pCard = PokemonCard(poke)
+            button =pCard.CreatePokeButton(poke, pokeIDName[poke])
+            self.ids.trainer.add_widget(button)
+
+    pass
 
 class PokePrimerApp(MDApp):
     
