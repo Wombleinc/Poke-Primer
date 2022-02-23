537d48f09afb751f27ffb3cbd9b15c1066d9b0f1
Christopher Duke <clduke2@my.waketech.edu>
2022-02-22 23:53:09 -0500

    Added functionality for "Add" button in PokeDex view

--- Trainer/scripts.py
+++ Trainer/scripts.py
@@ -2,6 +2,10 @@
 # TeamDex/scripts.py
 
 import csv
+import os
+import pandas
+from Trainer.classes import Pokemon
+from Trainer.classes import Team
 
 
 def save_team_to_csv(pokemon_team):
@@ -15,3 +19,21 @@ def save_team_to_csv(pokemon_team):
         for count, pokemon in enumerate(pokemon_team.members):
             writer.writerow([count + 1, pokemon.get_name(), pokemon.get_item(),
                              pokemon.get_move1(), pokemon.get_move2(), pokemon.get_move3(), pokemon.get_move4()])
+
+
+def load_pokemon_data():
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    # Define dataset path in reference to the root path
+    DATASET_PATH = os.path.join(ROOT_DIR, '..\\assets\\Pokemon_edit.csv')
+
+    # Read in dataset
+    return pandas.read_csv(DATASET_PATH)
+
+
+def create_pokemon_from_id(pokemon_id):
+    pokemon_data_frame = load_pokemon_data()
+    return Pokemon(str(pokemon_data_frame.get("name")[pokemon_id]))
+
+
+def add_from_id_to_team(pokemon_id, team):
+    team.add_pokemon(create_pokemon_from_id(pokemon_id))
