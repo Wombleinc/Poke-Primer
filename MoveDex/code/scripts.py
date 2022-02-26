57655c938e6905d4ab82649907381662e3ed2b81
Chad <chadjpeterson21@gmail.com>
2022-02-26 11:36:57 -0500

    fixed tests

--- MoveDex/code/scripts.py
+++ MoveDex/code/scripts.py
@@ -8,5 +8,15 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATASET_PATH = os.path.join(ROOT_DIR, '..\\assets\\move_dataset.csv')
 
 # Read in dataset
-move_data_frame = pandas.read_csv(DATASET_PATH)
+move_data_frame_num = pandas.read_csv(DATASET_PATH, index_col='#')
+move_data_frame_move = pandas.read_csv(DATASET_PATH, index_col='Name')
 
+# brainstorming pseudocode for interaction script:
+
+# while user in MoveDex:
+# user_query = bla (input from kivy)
+# if move_data_frame[user_query] exists:
+# output(move_data_frame[user_query])
+
+print(move_data_frame_num.iloc[0])
+print(move_data_frame_move.loc['Pound'])
