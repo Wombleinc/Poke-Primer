2a58e8401e0e735922b00a7e528befeba2693d37
AJohnsonWakeTech <75625961+AJohnsonWakeTech@users.noreply.github.com>
2022-03-10 20:46:55 -0500

    Made it look nicer

--- App/main.py
+++ App/main.py
@@ -2,13 +2,17 @@ from kivy.uix.label import Label
 import kivy
 from kivy.app import App
 from kivymd.app import MDApp
+from kivy.core.text import LabelBase
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.popup import Popup
 from kivy.uix.button import Button
 from kivy.core.window import Window
 from functools import partial
+import sys
+sys.path.append('/Poke-Primer-main/')
 
 from Trainer.classes import Team
 from Trainer.scripts import all_pokemon_id_list, all_pokemon_name_list
@@ -16,9 +20,8 @@ from Trainer.scripts import all_pokemon_id_list, all_pokemon_name_list
 from card import Card
 
 from ItemDex.scripts import get_item_id_list, get_item_name_list
-
-Window.clearcolor = (1, 1, 1, 1)
-Window.size = (540, 1140)
+LabelBase.register(name='PokeFont', fn_regular='Pokemon Classic.ttf')
+Window.size = (350, 700)
 
 """ 
     This class creates a card for each pokemon/move/item that stores its id
@@ -52,9 +55,15 @@ class PokemonCard(BoxLayout):
 
     def CreatePokeButton(self, poke_id, poke_name):
         self.id = poke_id
-        button = Button(text=str(poke_id) + " " + poke_name, size_hint=(.95, None), size=(100,40), pos_hint={'center_x': .5})
+        full_button = BoxLayout(orientation='horizontal', size_hint=(None,None), height=50)
+        lbl_number = Label(text=str(poke_id),font_size=18, size_hint=(None, None),size=(60,40), pos_hint={'x':.1}, font_name="PokeFont", color=(.17, .23, .2, 1))
+        button = Button(text=poke_name, size_hint=(None, None), size=(220,40), pos_hint={'x': .1}, background_normal='dark_bg.jpg', background_down='light_bg.jpg', color=(.64, .72, .66, 1), font_name="PokeFont")
         button.bind(on_release=partial(self.GetCardData))
-        return button
+        ball_button = Button(background_normal='icon_ball.png', size_hint=(None, None), size=(40,40), pos={50,0}, border=(0, 0, 0, 0))
+        full_button.add_widget(lbl_number)
+        full_button.add_widget(button)
+        full_button.add_widget(ball_button)
+        return full_button
 
     def AddToTrainer(self, *args):
         # This function now saves teams to json instead of csv
@@ -81,8 +90,8 @@ class PokeDex(BoxLayout):
 
         for poke in pokeIDList:
             pCard = PokemonCard(poke)
-            button = pCard.CreatePokeButton(poke, pokeIDName[poke])
-            self.ids.poke.add_widget(button)
+            full_button = pCard.CreatePokeButton(poke, pokeIDName[poke])
+            self.ids.poke_grid.add_widget(full_button)
     pass
 
 """ 
@@ -153,6 +162,9 @@ class Trainer(BoxLayout):
 class PokePrimerApp(MDApp):
 
     def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Gray"
+        self.theme_cls.accent_palette = "Red" 
         return Builder.load_file("main.kv")
 
 
