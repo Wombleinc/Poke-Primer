6626332d3e01badcc1e60b01bca44dccd6f7c059
Chad <53791319+wombatpandaa@users.noreply.github.com>
2022-04-13 22:00:28 -0400

    moves card.py stuff

--- App/card.py
+++ App/card.py
@@ -3,6 +3,7 @@
     and put it in a format the UI can use.
     The asset ID is being stored in self.number
 """
+from MoveDex.code.scripts import get_move_accuracy, get_move_category, get_move_name, get_move_power, get_move_pp, get_move_type
 from Trainer.scripts import get_pokemon_genus, get_pokemon_name
 from ItemDex.request_item import get_item_name, get_item_effect, get_item_category
 
@@ -24,4 +25,8 @@ class CardMove:
     def __init__(self, id):
         self.number = str(id)
         self.name = get_move_name(id)
-        self.description = get_move_genus(id)
+        self.power = get_move_power(id)
+        self.accuracy = get_move_accuracy(id)
+        self.type = get_move_type(id)
+        self.category = get_move_category(id)
+        self.pp = get_move_pp(id)
--- MoveDex/code/scripts.py
+++ MoveDex/code/scripts.py
@@ -1,8 +1,12 @@
 import json
 import sys
 
-
 sys.path.append('/Poke-Primer-main/')
+sys.path.append('..')
+sys.path.append('../../MoveDex')
+
+from MoveDex.code.move import Move
+
 
 def build_move_dict():
     with open("PokemonData/moves.json", 'r') as move_json:
@@ -37,8 +41,82 @@ def get_move_master_list():
         move_list_sort.append(dict(id=move_dict['move'][i]['#'],name=move_dict['move'][i]['Name'],type=move_dict['move'][i]['Type']))
     return move_list_sort
 
-# def get_move_master_list():
-#     move_dict = build_move_dict()
-#     move_list = []
-#     for i in range(0,850):
-#         move_list.append(dict(id=move_dict["move"][i][""]))
+def get_move_move_list():
+    move_dict = build_move_dict()
+    move_list = []
+    for i in range(0,850):
+        generic_move = Move(
+            move_dict['move'][i]['#'],
+            move_dict['move'][i]['Name'],
+            move_dict['move'][i]['Type'],
+            move_dict['move'][i]['Category'],
+            move_dict['move'][i]['PP'],
+            move_dict['move'][i]['Power'],
+            move_dict['move'][i]['Accuracy'],
+            move_dict['move'][i]['Gen'],
+        )
+        move_list.append(generic_move)
+    return move_list
+
+# cards stuff - these grab the move name, power, acuracy,
+# type, category, and PP and pass them to cards.py
+
+def get_move_name(id):
+    move_list = get_move_move_list()
+    move_name = None
+
+    for move in move_list:
+        if move.move_id == id:
+            move_name = move.name
+
+    return move_name
+
+def get_move_power(id):
+    move_list = get_move_move_list()
+    move_power = None
+
+    for move in move_list:
+        if move.move_id == id:
+            move_power = move.power
+
+    return move_power
+
+def get_move_accuracy(id):
+    move_list = get_move_move_list()
+    move_accuracy = None
+
+    for move in move_list:
+        if move.move_id == id:
+            move_accuracy = move.accuracy
+
+    return move_accuracy
+
+def get_move_type(id):
+    move_list = get_move_move_list()
+    move_type = None
+
+    for move in move_list:
+        if move.move_id == id:
+            move_type = move.move_type
+
+    return move_type
+
+def get_move_category(id):
+    move_list = get_move_move_list()
+    move_category = None
+
+    for move in move_list:
+        if move.move_id == id:
+            move_category = move.category
+
+    return move_category
+
+def get_move_pp(id):
+    move_list = get_move_move_list()
+    move_pp = None
+
+    for move in move_list:
+        if move.move_id == id:
+            move_pp = move.pp
+
+    return move_pp
