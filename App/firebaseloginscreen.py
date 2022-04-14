4234a885a488c939c336e959e0502e1af622c453
WombleInc <joshkw10691@gmail.com>
2022-04-13 20:03:49 -0400

    Update firebaseloginscreen.py

--- App/firebaseloginscreen.py
+++ App/firebaseloginscreen.py
@@ -14,7 +14,6 @@ import progressspinner
 
 # Load the kv files
 folder = os.path.dirname(os.path.realpath(__file__))
-Builder.load_file(folder + "/themedwidgets.kv")
 Builder.load_file(folder + "/signinscreen.kv")
 Builder.load_file(folder + "/signupscreen.kv")
 Builder.load_file(folder + "/welcomescreen.kv")
