0ad39e2a20fef7b375667cdb50a1f987a8415824
clduke2 <71196463+clduke2@users.noreply.github.com>
2022-04-14 00:03:54 -0400

    Moves and trainer integration

--- MoveDex/code/scripts.py
+++ MoveDex/code/scripts.py
@@ -2,49 +2,24 @@ import json
 import sys
 
 sys.path.append('/Poke-Primer-main/')
-sys.path.append('..')
-sys.path.append('../../MoveDex')
 
 from MoveDex.code.move import Move
 
-
 def build_move_dict():
+    """Builds a dictionary of move data, including ID number, Name, Type, Category,
+    PP, Power, Accuracy, and Gen, to save space doing this same process in every method."""
     with open("PokemonData/moves.json", 'r') as move_json:
         move_dict = json.load(move_json)
     return move_dict
-
-def get_move_id_list():
-    move_dict = build_move_dict()
-    move_id_list = []
-    for i in range(0,850):
-        move_id_list.append(move_dict['move'][i]['#'])
-    return move_id_list
-
-def get_move_name_list():
-    move_dict = build_move_dict()
-    move_name_list = []
-    for i in range(0,850):
-        move_name_list.append(move_dict['move'][i]['Name'])
-    return move_name_list
-
-def get_move_type_list():
-    move_dict = build_move_dict()
-    move_type_list = []
-    for i in range(0,850):
-        move_type_list.append(move_dict['move'][i]['Type'])
-    return move_type_list
-
-def get_move_master_list():
-    move_dict = build_move_dict()
-    move_list_sort = []
-    for i in range(0,850):
-        move_list_sort.append(dict(id=move_dict['move'][i]['#'],name=move_dict['move'][i]['Name'],type=move_dict['move'][i]['Type']))
-    return move_list_sort
-
-def get_move_move_list():
+    
+def get_move_list():
+    """Creates a list of Move objects from the dictionary created in build_move_dict(), 
+    with the attributes properly associated."""
     move_dict = build_move_dict()
     move_list = []
-    for i in range(0,850):
+    # uncomment below and comment "for i in range(0,165):" for all moves across all current generations
+    # for i in range(0,850):
+    for i in range(0,165): # limited to generation 1 for quicker access time
         generic_move = Move(
             move_dict['move'][i]['#'],
             move_dict['move'][i]['Name'],
@@ -58,11 +33,37 @@ def get_move_move_list():
         move_list.append(generic_move)
     return move_list
 
-# cards stuff - these grab the move name, power, acuracy,
-# type, category, and PP and pass them to cards.py
+# Accessed by main.py
+
+def get_move_id_list():
+    """Gets a list of move ID's alone, using the move_list of Move objects set up in get_move_list()."""
+    move_list = get_move_list()
+    move_id_list = []
+    for move in move_list:
+        move_id_list.append(move.move_id)
+    return move_id_list
+
+def get_move_name_list():
+    """Gets a list of move names alone, using the move_list of Move objects set up in get_move_list()."""
+    move_list = get_move_list()
+    move_name_list = []
+    for move in move_list:
+        move_name_list.append(move.name)
+    return move_name_list
+
+def get_move_type_list():
+    """Gets a list of move types alone, using the move_list of Move objects set up in get_move_list()."""
+    move_list = get_move_list()
+    move_type_list = []
+    for move in move_list:
+        move_type_list.append(move.move_type)
+    return move_type_list
+
+# Accessed by cards.py
 
 def get_move_name(id):
-    move_list = get_move_move_list()
+    """Grabs a specific move name by ID, using the move_list of Move objects set up in get_move_list()."""
+    move_list = get_move_list()
     move_name = None
 
     for move in move_list:
@@ -72,7 +73,8 @@ def get_move_name(id):
     return move_name
 
 def get_move_power(id):
-    move_list = get_move_move_list()
+    """Grabs a specific move power by ID, using the move_list of Move objects set up in get_move_list()."""
+    move_list = get_move_list()
     move_power = None
 
     for move in move_list:
@@ -82,7 +84,8 @@ def get_move_power(id):
     return move_power
 
 def get_move_accuracy(id):
-    move_list = get_move_move_list()
+    """Grabs a specific move accuracy by ID, using the move_list of Move objects set up in get_move_list()."""
+    move_list = get_move_list()
     move_accuracy = None
 
     for move in move_list:
@@ -92,7 +95,8 @@ def get_move_accuracy(id):
     return move_accuracy
 
 def get_move_type(id):
-    move_list = get_move_move_list()
+    """Grabs a specific move type by ID, using the move_list of Move objects set up in get_move_list()."""
+    move_list = get_move_list()
     move_type = None
 
     for move in move_list:
@@ -102,7 +106,9 @@ def get_move_type(id):
     return move_type
 
 def get_move_category(id):
-    move_list = get_move_move_list()
+    """Grabs a specific move category (whether the move is physical or special) by ID, using the move_list of 
+    Move objects set up in get_move_list()."""
+    move_list = get_move_list()
     move_category = None
 
     for move in move_list:
@@ -112,7 +118,8 @@ def get_move_category(id):
     return move_category
 
 def get_move_pp(id):
-    move_list = get_move_move_list()
+    """Grabs a specific move PP (Power Points) by ID, using the move_list of Move objects set up in get_move_list()."""
+    move_list = get_move_list()
     move_pp = None
 
     for move in move_list:
