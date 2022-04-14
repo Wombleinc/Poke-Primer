5cdaa12b2ddca6317f67af14bf7ca7613ed5fc73
WombleInc <joshkw10691@gmail.com>
2022-04-13 23:10:41 -0400

    Update main.py

--- App/main.py
+++ App/main.py
@@ -503,6 +503,16 @@ class ScreenManagement(ScreenManager):
     pass
 
 class PokePrimerApp(App):
+  
+  user_idToken = ""
+    local_id = ""
+
+    def display_user_tokens(self):
+        self.root.ids.the_label.text = "local_id: " + self.local_id + "\n user_idToken: " + self.user_idToken
+
+    def sign_out(self):
+        self.root.ids.firebase_login_screen.log_out()
+        self.root.current = 'firebase_login_screen'
 
     def build(self):
         Builder.load_file("main.kv")
