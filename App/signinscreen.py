c1201163c5ab1666a0f4629f8f8015c871f1211a
clduke2 <71196463+clduke2@users.noreply.github.com>
2022-05-02 02:05:18 -0400

    Delete App directory

--- App/signinscreen.py
+++
@@ -1,8 +0,0 @@
-from kivy.uix.screenmanager import Screen, SlideTransition
-
-class SignInScreen(Screen):
-    def go_back(self):
-        self.parent.transition = SlideTransition(direction="right")
-        self.parent.current = self.parent.current = "welcome_screen"
-        self.parent.transition = SlideTransition(direction="left")
-
