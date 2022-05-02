0db01a503e6a4fadf8d4e1ce65d20a3753fe41d1
clduke2 <71196463+clduke2@users.noreply.github.com>
2022-05-02 02:05:51 -0400

    Delete MoveDex directory

--- MoveDex/code/test.py
+++
@@ -1,8 +0,0 @@
-from move import Move
-import scripts
-
-test_move = Move()
-Move.id, Move.name, Move.accuracy, Move.pp, Move.priority, Move.power = 1, "", 1, 1, 1, 1
-# print(test_move)
-
-print(scripts.move_data_frame)
